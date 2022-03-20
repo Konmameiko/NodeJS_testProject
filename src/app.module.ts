325ebb95d46bee7a8fc8855bf5e786459895a654
konmameiko <a13224010118@163.com>
2022-03-20 22:32:48 +0800

    connect  database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+// 引入数据库的及配置文件
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostsModule } from './posts/posts.module';
 
 // 根模块
